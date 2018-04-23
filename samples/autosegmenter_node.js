6269d3285496e2374d1929a1b4553ecb649b4071
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-23 19:12:21 +1000

    Initial commit

---
+++ samples/autosegmenter_node.js
@@ -0,0 +1,33 @@
+/* Author: David L. Irving
+ * This example is intended to be ran with node.js
+ *
+ * Please set your ALVEO_API_KEY environment variable before use.
+ *
+ * This example demonstrates how to use the transcriber services
+ *  class to automatically segment an audiofile hosted on the Alveo
+ *  API.
+ *
+ * Makes use of ES2017 async to simplify syntax.
+ *
+ * Execution:
+ *  - `babel-node autosegmenter_node.js`
+ * */
+
+import { TranscriberServices } from '../src/transcriber-services';
+
+var alveots = new TranscriberServices(
+  {
+    apiUrl: 'https://segmenter.apps.alveo.edu.au'
+  }
+);
+
+async function autosegment() {
+  try {
+    var segments = await alveots.segment('https://app.alveo.edu.au/catalog/austalk/1_1274_2_7_001/document/1_1274_2_7_001-ch6-speaker16.wav');
+    console.log(segments);
+  } catch(error) {
+    console.log(error.message);
+  }
+}
+
+autosegment();
